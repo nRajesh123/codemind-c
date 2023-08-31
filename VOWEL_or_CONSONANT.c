69d70cddc9c8d199d57b8289555e56012a4877e4
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-08-31 08:55:15 -0700

    #include<stdio.h> int main() {  char rr;scanf("%c",&rr);  if(rr=='a'||rr=='A'||rr=='e'||rr=='E'||rr=='I'||rr=='i'||rr=='o'||rr=='O'||rr=='u'||rr=='U')  printf("VOWEL");  else printf("CONSONANT"); }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+ char rr;scanf("%c",&rr);
+ if(rr=='a'||rr=='A'||rr=='e'||rr=='E'||rr=='I'||rr=='i'||rr=='o'||rr=='O'||rr=='u'||rr=='U')
+ printf("VOWEL");
+ else printf("CONSONANT");
+}
