385edf65f913d06137f9c1733a81c7097b9dc6b1
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-13 08:56:38 -0700

    Update Hermoine_and_Spells.c

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,3 +1,9 @@
+/*Hermoine has three spells. Their powers are A, B, and C respectively Initially, Hermaine has 0 hit points, and if she uses a spell with power P, then her number of hit points increases
+by P.
+Before going to sleep, Hermaine wants to use exactly two spells out of these three: Find the maximum number of hit points Hermoine can have after using the spells
+It is quaranteed that A, B and C voules are distinct*/
+
+
 #include<stdio.h>
 int main(){
     int a,b,c;
