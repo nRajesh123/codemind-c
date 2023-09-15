8618010895d3f0dc272eace0bff4f4a1fd488936
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-15 09:17:53 -0700

    Update Wanda_and_Bread.c

--- Wanda_and_Bread.c
+++ Wanda_and_Bread.c
@@ -1,3 +1,6 @@
+/*Wanda loves bread. She has N loaves of bread, all of which expire after exactly M days. 
+She can eat upto K loaves of bread in a day. Can she eat all the loaves of bread before they expire?*/
+
 #include<stdio.h>
 int main(){
     int n,m,k;
