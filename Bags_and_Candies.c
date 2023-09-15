5a72a0196daa2b73ff641058bc1d982c93c1c921
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-15 09:26:31 -0700

    Update Bags_and_Candies.c

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -1,3 +1,6 @@
+/*Rom received N candies on his birthday. He wants to put these candies in some bags. A bag has K pockets and each pocket can hold at most M candies. 
+Find the minimum number of bags Ram needs so that he can put every candy into a bag*/
+
 #include<stdio.h>
 int main(){
     int k,m,n,r,c;
