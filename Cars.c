431dc3a6edd9a3f8afe7738ca39f89cedb43b6d6
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-15 09:28:38 -0700

    Update Cars.c

--- Cars.c
+++ Cars.c
@@ -1,3 +1,7 @@
+/*A single car can accommodate at most 4 peopleN friends want to go to a restaurant for a party. 
+Find the minimum number of cars required to accommodate all the friends*/
+
+
 #include<stdio.h>
 int main(){
     int n,rp,p;
