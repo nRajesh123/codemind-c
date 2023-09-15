89b699b03bd084ff582b2533746cf6c8b8948739
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-15 09:24:16 -0700

    Update Blackjack.c

--- Blackjack.c
+++ Blackjack.c
@@ -1,3 +1,8 @@
+/*King is playing a variant of Blackjack, where 3 numbers are drawn and each number lies between 1 and 10 (with both 1 and 10 inclusive). 
+King wins the game when the sum of these 3 numbers is exactly 21.
+Given the first two numbers A and B, that have been drawn by King, what should be 3-rd number that should be drawn by the King in order to win the game?
+Note that it is possible that King cannot win the game, no matter what is the 3-rd number. In such cases, report l'as the answer */
+
 #include<stdio.h>
 int main(){
     int a,b,n;
